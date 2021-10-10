370656e956b0045c3b0ce5892ce35d358915f6b7
saayeeem <chysayem9@gmail.com>
2021-10-10 18:10:06 +0600

    Firebase Deployment

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+export const config = {
+  apiKey: "AIzaSyCWKICNFcqP2LUQRYBHIfDjUXBiGE3Q5rw",
+  authDomain: "confusionserver-cc768.firebaseapp.com",
+  projectId: "confusionserver-cc768",
+  storageBucket: "confusionserver-cc768.appspot.com",
+  messagingSenderId: "123163719331",
+  appId: "1:123163719331:web:00d964ee79828f745c7116",
+  measurementId: "G-MJS34D8FR6",
+};
---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { config } from './config';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+export const fireauth = firebase.auth;
+
+const settings = {timestampsInSnapshots: true};
+firebase.firestore().settings(settings);
+export const firestore = firebase.firestore();
+
+export const firebasestore = firebase.firestore;
